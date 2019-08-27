17218e536674f0f14a7b15cd7cbd60e596643ee7
snipercool <sniper.cool@hotmail.com>
2019-08-27 22:52:24 +0200

    primus

--- primus/primus.js
+++ primus/primus.js
@@ -3,9 +3,12 @@ const Primus = require('primus');
 let go = (server) => {
    let primus = new Primus(server, {});
 
-    primus.on('connection', () =>{
+    primus.on('connection', (spark) =>{
         console.log('sparky');
         
+        spark.on('data', (data) => {
+            primus.write(data);
+        });
     });
 
 }
